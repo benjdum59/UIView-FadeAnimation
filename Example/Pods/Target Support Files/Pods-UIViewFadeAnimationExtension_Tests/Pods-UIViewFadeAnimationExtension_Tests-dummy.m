d65e6a8850f21aae0cd6041bf44e199fc077ee7d
benjamindum <benjamin.dumont@ineat-conseil.fr>
2016-02-15 16:22:05 +0100

    Rename

---
+++ Example/Pods/Target Support Files/Pods-UIViewFadeAnimationExtension_Tests/Pods-UIViewFadeAnimationExtension_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIViewFadeAnimationExtension_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UIViewFadeAnimationExtension_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-UIViewFadeAnimationExtension_Tests/Pods-UIViewFadeAnimationExtension_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_UIViewFadeAnimationExtension_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UIViewFadeAnimationExtension_TestsVersionString[];
+
